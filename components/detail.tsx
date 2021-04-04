cbfd4c914369bf49bb120aa52c5b91cee4aad92b
Nathan Kane <kanenathan213@gmail.com>
2021-04-04 15:35:15 -0400

    center text, increase letter spacing of name

--- components/detail.tsx
+++ components/detail.tsx
@@ -8,7 +8,7 @@ export default function Detail({ handleBottomScolledTo }: Props) {
   return (
     <section className="flex-col flex items-center pb-8">
       <h4 className="font-bold text-xl">About me</h4>
-      <p className="text-center md:text-left text-lg mt-5 md:pl-8 mb-8 max-w-xl">
+      <p className="text-center md:text-center text-lg mt-5 md:pl-8 mb-8 max-w-xl">
         I've worked with React, React Native, Node, and Python. I'm on the
         engineering team at Petal, a credit card startup based in New York City.
         When my laptop is shut, I like hiking, scuba diving, and devouring sushi
--- components/intro.tsx
+++ components/intro.tsx
@@ -7,10 +7,10 @@ type Props = {
 export default function Intro({ hasBeenScrolled }: Props) {
   return (
     <section className="min-h-screen flex-col flex items-center justify-center">
-      <h1 className="text-4xl md:text-5xl tracking-tighter font-thin uppercase font-body leading-tight md:pr-8">
+      <h1 className="text-center text-4xl md:text-5xl tracking-wide font-thin uppercase font-body leading-tight">
         Nathan Kane
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <h4 className="text-center md:text-center text-lg mt-5 md:pl-8">
         I'm a software engineer who believes in building world-positive
         products.
       </h4>
