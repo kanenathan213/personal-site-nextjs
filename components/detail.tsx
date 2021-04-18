72474b25830a82dc24d78ec9d59c16148205dd94
Nathan Kane <kanenathan213@gmail.com>
2021-04-18 14:13:04 -0400

    new copy and links

--- components/detail.tsx
+++ components/detail.tsx
@@ -9,10 +9,13 @@ export default function Detail({ handleBottomScolledTo }: Props) {
     <section className="flex-col flex items-center pb-8">
       <h4 className="font-bold text-xl">About me</h4>
       <p className="text-center md:text-center text-lg mt-5 md:pl-8 mb-8 max-w-xl">
-        I've worked with React, React Native, Node, and Python. I'm on the
-        engineering team at Petal, a credit card startup based in New York City.
-        When my laptop is shut, I like hiking, scuba diving, and devouring sushi
-        (not at the same time).
+        The technologies I've worked with most are React / React Native,
+        TypeScript, and Python. Right now, I'm on the engineering team at{' '}
+        <a className="underline" href="https://www.petalcard.com">
+          Petal
+        </a>
+        . When my laptop is shut, I like hiking, scuba diving, and devouring
+        sushi (not at the same time).
       </p>
       <div className="flex-col flex items-center">
         <h4 className="font-bold text-xl mb-4">Let's chat</h4>
