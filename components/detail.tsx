08030eaa22941e967c73f06ad460318950a9bdce
Nathan Kane <kanenathan213@gmail.com>
2022-09-17 09:30:35 -0400

    update current company, tech, and description

--- components/detail.tsx
+++ components/detail.tsx
@@ -9,15 +9,19 @@ export default function Detail({ handleBottomScolledTo }: Props) {
     <section className="flex-col flex items-center pb-8">
       <h4 className="font-bold text-xl">About me</h4>
       <p className="text-center md:text-center text-lg mt-5 md:pl-8 mb-8 max-w-xl">
-        The technologies I've worked with most are React / React Native,
-        TypeScript, and Python. Right now, I'm on the engineering team at{' '}
+        The technologies I've worked with most are React / React Native, Node,
+        TypeScript, and Python. Right now, I'm a software engineer at{' '}
+        <a className="underline" href="https://www.sesolabor.com">
+          Seso
+        </a>
+        . Previous, I led the product engineering organization at{' '}
         <a className="underline" href="https://www.petalcard.com">
           Petal
         </a>
-        . When my laptop is shut, I like hiking, scuba diving, and devouring
-        sushi (not at the same time).
+        . When my laptop is shut, I like going hiking, scuba diving, and
+        devouring sushi (not at the same time).
       </p>
-      <div className="flex-col flex items-center">
+      <div className="flex-col flex items-center mt-8">
         <h4 className="font-bold text-xl mb-4">Let's chat</h4>
         <a className="underline" href="https://www.linkedin.com/in/nathankane/">
           LinkedIn
