e2b865b8bd1bb88ee6d1b9670e187d0d68b4ad8e
Nathan Kane <kanenathan213@gmail.com>
2024-02-24 14:46:41 -0500

    update email

--- components/detail.tsx
+++ components/detail.tsx
@@ -9,8 +9,8 @@ export default function Detail({ handleBottomScolledTo }: Props) {
     <section className="flex-col flex items-center pb-8">
       <h4 className="font-bold text-xl">About me</h4>
       <p className="text-center md:text-center text-lg mt-5 md:pl-8 mb-8 max-w-xl">
-        The technologies I've worked with most are React / React Native, Node,
-        TypeScript, and Python. Right now, I'm a software engineer at{' '}
+        The technologies I've worked with most are React / React Native, Node
+        (Express), TypeScript, and Python. Right now, I'm a software engineer at{' '}
         <a className="underline" href="https://www.sesolabor.com">
           Seso
         </a>
@@ -18,8 +18,13 @@ export default function Detail({ handleBottomScolledTo }: Props) {
         <a className="underline" href="https://www.petalcard.com">
           Petal
         </a>
+<<<<<<< Updated upstream
         . When my laptop is shut, I like going hiking, scuba diving, and
         devouring sushi (not at the same time).
+=======
+        asdf . When my laptop is shut, I like going hiking, scuba diving, and
+        devouring sushi (not at the same time).asdf
+>>>>>>> Stashed changes
       </p>
       <div className="flex-col flex items-center mt-8">
         <h4 className="font-bold text-xl mb-4">Let's chat</h4>
@@ -29,7 +34,7 @@ export default function Detail({ handleBottomScolledTo }: Props) {
         <a className="underline" href="https://github.com/kanenathan213">
           Github
         </a>
-        <p data-test-id="email_address">hi@nathanrkane.com</p>
+        <p data-test-id="email_address">kanenathan213@gmail.com</p>
         <Waypoint onEnter={handleBottomScolledTo} />
       </div>
     </section>
